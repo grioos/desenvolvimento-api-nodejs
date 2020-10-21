14c14740059ae6fbdf07c23114efcf7c3fa65e83
Gabriel Rios <gabriel.al.rio@gmail.com>
2020-10-21 13:22:27 -0300

    Refactoring callbacks to promise

--- 02-nodejs/index.js
+++ 02-nodejs/index.js
@@ -1,20 +1,27 @@
-function obterUsuario(callback) {
-    setTimeout(() => {
-        return callback(null, {
-            id: 1,
-            nome: 'Aladin',
-            dataNascimento: new Date()
-        })
-    }, 1000)
+const util = require('util')
+const obterEnderecoAsync = util.promisify(obterEndereco)
+
+function obterUsuario() {
+    return new Promise(function resolvePromise(resolve, reject) {
+        setTimeout(() => {
+            return resolve({
+                id: 1,
+                nome: 'Aladin',
+                dataNascimento: new Date()
+            })
+        }, 1000)
+    })
 }
 
-function obterTelefone(idUsuario, callback) {
-    setTimeout(() => {
-        return callback(null, {
-            telefone: '1190028922',
-            ddd: 11
-        })
-    }, 2000)
+function obterTelefone(idUsuario) {
+    return new Promise(function resolvePromise(resolve, reject) {
+        setTimeout(() => {
+            return resolve({
+                telefone: '90028922',
+                ddd: 11
+            })
+        }, 2000)
+    })
 }
 
 function obterEndereco(idUsuario, callback) {
@@ -30,30 +37,65 @@ function resolverUsuario(erro, usuario) {
     console.log('usuario', usuario)
 }
 
-obterUsuario(function resolverUsuario(erro, usuario) {
-    if (erro) {
-        console.error('Deu ruim em usuário', erro)
-        return
-    }
+const usuarioPromise = obterUsuario()
 
-    obterTelefone(usuario.id, function resolverTelefone(erro1, telefone) {
-        if (erro1) {
-            console.error('Deu ruim em telefone', erro1)
-            return
-        }
-
-        obterEndereco(usuario.id, function resolverEndereco(erro2, endereco) {
-            if (erro2) {
-                console.error('Deu ruim em endereço', erro2)
-                return
+usuarioPromise
+    .then(usuario => {
+        return obterTelefone(usuario.id)
+            .then(function resolverTelefone(resultaldo) {
+                return {
+                    usuario: {
+                        nome: usuario.nome,
+                        id: usuario.id
+                    },
+                    telefone: resultaldo
+                }
+            })
+    })
+    .then(resultado => {
+        const endereco = obterEnderecoAsync(resultado.usuario.id)
+        return endereco.then(function resolveEndereco(endereco) {
+            return {
+                usuario: resultado.usuario,
+                telefone: resultado.telefone,
+                endereco: endereco
             }
-
-            console.log(`
-                Nome: ${usuario.nome},
-                Endereço: ${endereco.rua}, ${endereco.numero},
-                Telefone: (${telefone.ddd}) ${telefone.telefone}
-            `)
         })
     })
-})
-console.log('Hi')
+    .then(resultado => {
+        console.log(`
+            Nome: ${resultado.usuario.nome},
+            Endereco: ${resultado.endereco.rua}, ${resultado.endereco.numero},
+            Telefone: (${resultado.telefone.ddd}) ${resultado.telefone.telefone}
+        `)
+    })
+    .catch(erro => {
+        console.error('Deu ruim', erro)
+    })
+
+// obterUsuario(function resolverUsuario(erro, usuario) {
+//     if (erro) {
+//         console.error('Deu ruim em usuário', erro)
+//         return
+//     }
+
+//     obterTelefone(usuario.id, function resolverTelefone(erro1, telefone) {
+//         if (erro1) {
+//             console.error('Deu ruim em telefone', erro1)
+//             return
+//         }
+
+//         obterEndereco(usuario.id, function resolverEndereco(erro2, endereco) {
+//             if (erro2) {
+//                 console.error('Deu ruim em endereço', erro2)
+//                 return
+//             }
+
+//             console.log(`
+//                 Nome: ${usuario.nome},
+//                 Endereço: ${endereco.rua}, ${endereco.numero},
+//                 Telefone: (${telefone.ddd}) ${telefone.telefone}
+//             `)
+//         })
+//     })
+// })
