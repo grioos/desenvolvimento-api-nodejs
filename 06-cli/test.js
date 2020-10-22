865b22b347e8b921b7e61b6795c4764361547385
Gabriel Rios <gabriel.al.rio@gmail.com>
2020-10-22 13:59:30 -0300

    Manipulating archives - UPDATE

--- 06-cli/test.js
+++ 06-cli/test.js
@@ -1,14 +1,20 @@
-const { deepStrictEqual, ok } = require('assert')
+const { deepStrictEqual } = require('assert')
 const database = require('./database')
 const DEFAULT_ITEM_CADASTRAR = {
     nome: 'Flash',
     poder: 'Speed',
     id: 1
 }
+const DEFAULT_ITEM_ATUALIZAR = {
+    nome: 'Laterna Verde',
+    poder: 'Energia do Anel',
+    id: 2
+}
 
 describe('Suite de manipulação de Herois', () => {
     before(async () => {
         await database.cadastrar(DEFAULT_ITEM_CADASTRAR)
+        await database.cadastrar(DEFAULT_ITEM_ATUALIZAR)
     })
 
     it('Deve pesquisar um heroi usando arquivos', async () => {
@@ -20,15 +26,36 @@ describe('Suite de manipulação de Herois', () => {
 
     it('Deve cadastrar um heroi, usando arquivos', async () => {
         const expected = DEFAULT_ITEM_CADASTRAR
-        const resultado = await database.cadastrar(DEFAULT_ITEM_CADASTRAR)
+
+        await database.cadastrar(DEFAULT_ITEM_CADASTRAR)
+
         const [atual] = await database.listar(DEFAULT_ITEM_CADASTRAR.id)
 
-        ok(atual, expected)
+        deepStrictEqual(atual, expected)
     })
 
-    it('Deve remover um heroi por id', async () => {
+    it('Deve remover um heroi pelo id', async () => {
         const expected = true
         const resultado = await database.remover(DEFAULT_ITEM_CADASTRAR.id)
+
+        deepStrictEqual(resultado, expected)
+    })
+
+    it('Deve atualizar um heroi pelo id', async () => {
+        const expected = {
+            ...DEFAULT_ITEM_ATUALIZAR,
+            nome: 'Batman',
+            poder: 'Dinheiro'
+        }
+
+        const novoDado = {
+            nome: 'Batman',
+            poder: 'Dinheiro'
+        }
+
+        await database.atualizar(DEFAULT_ITEM_ATUALIZAR.id, novoDado)
+
+        const [resultado] = await database.listar(DEFAULT_ITEM_ATUALIZAR.id)
 
         deepStrictEqual(resultado, expected)
     })
