26c30bcd4a735cb5955da7c2f0c56c29d07b89a2
Gabriel Rios <gabriel.al.rio@gmail.com>
2020-10-26 14:56:41 -0300

    Mongoose

--- 08-multi-db-postgres-mongodb/src/mongodbExample.js
+++ 08-multi-db-postgres-mongodb/src/mongodbExample.js
@@ -6,32 +6,8 @@ const state = connection.readyState
 
 console.log('state', state)
 
-const heroiSchema = new Mongoose.Schema({
-    nome: {
-        type: String,
-        required: true
-    },
-
-    poder: {
-        type: String,
-        required: true
-    },
-
-    insertedAt: {
-        type: Date,
-        dafault: new Date()
-    }
-})
-
-const model = Mongoose.model('herois', heroiSchema)
 
 async function main() {
-    const resultCadastrar = await model.create({
-        nome: 'Batman',
-        poder: 'Dinheiro'
-    })
-
-    console.log('Result cadastrar', resultCadastrar)
 
     const listItens = await model.find()
 
