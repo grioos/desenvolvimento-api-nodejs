b6b5a87ce793fbc80f81c61347259c4a02bea92f
Gabriel Rios <gabriel.al.rio@gmail.com>
2020-11-04 11:52:00 -0300

    Heroes list - Validation with Joi

---
+++ 15-hapi-listar-joi/src/tests/apiHeroes.test.js
@@ -0,0 +1,71 @@
+const assert = require('assert')
+const api = require('./../api')
+let app = {}
+
+describe('Suite de testes da API Heroes', () => {
+    before(async () => {
+        app = await api
+    })
+
+    it('Listar /herois', async () => {
+        const result = await app.inject({
+            method: 'GET',
+            url: '/herois?skip=0&limit=10'
+        })
+
+        const dados = JSON.parse(result.payload)
+        const statusCode = result.statusCode
+
+        assert.deepStrictEqual(statusCode, 200)
+        assert.ok(Array.isArray(dados))
+    })
+
+    it('Listar /herois - deve retornar somente 3 registros', async () => {
+        const TAMANHO_LIMITE = 3
+        const result = await app.inject({
+            method: 'GET',
+            url: `/herois?skip=0&limit=${TAMANHO_LIMITE}`
+        })
+
+        const dados = JSON.parse(result.payload)
+        const statusCode = result.statusCode
+
+        assert.deepStrictEqual(statusCode, 200)
+        assert.ok(dados.length === TAMANHO_LIMITE)
+    })
+
+    it('Listar /herois - deve retornar um erro com limit incorreto', async () => {
+        const TAMANHO_LIMITE = 'AEEE'
+        const result = await app.inject({
+            method: 'GET',
+            url: `/herois?skip=0&limit=${TAMANHO_LIMITE}`
+        })
+
+        const errorResult = {
+            "statusCode": 400,
+            "error": "Bad Request",
+            "message": "child \"limit\" fails because [\"limit\" must be a number]",
+            "validation": {
+                "source": "query",
+                "keys": ["limit"]
+            }
+        }
+
+        assert.deepStrictEqual(result.statusCode, 400)
+        assert.deepStrictEqual(result.payload, JSON.stringify(errorResult))
+    })
+
+    it('Listar /herois - deve filtrar um item', async () => {
+        const NAME = 'Flash'
+        const result = await app.inject({
+            method: 'GET',
+            url: `/herois?skip=0&limit=1000&nome=${NAME}`
+        })
+
+        const dados = JSON.parse(result.payload)
+        const statusCode = result.statusCode
+
+        assert.deepStrictEqual(statusCode, 200)
+        assert.deepStrictEqual(dados[0].nome, NAME)
+    })
+})
