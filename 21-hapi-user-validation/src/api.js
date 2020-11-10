d3f98d7e708c6822f4ea6e6bf1c70aa030573a17
Gabriel Rios <gabriel.al.rio@gmail.com>
2020-11-10 17:28:53 -0300

    User authentication and password hash

---
+++ 21-hapi-user-validation/src/api.js
@@ -0,0 +1,80 @@
+const Hapi = require('@hapi/hapi')
+const Inert = require('@hapi/inert')
+const Vision = require('@hapi/vision')
+const HapiSwagger = require('hapi-swagger')
+const Joi = require('joi')
+const HapiJwt = require('hapi-auth-jwt2')
+const Jwt = require('jsonwebtoken')
+const Context = require('./db/strategies/base/contextStrategy')
+const MongoDb = require('./db/strategies/mongodb/mongodb')
+const HeroiSchema = require('./db/strategies/mongodb/schemas/heroisSchema')
+const Postgres = require('./db/strategies/postgres/postgres')
+const UsuarioSchema = require('./db/strategies/postgres/schemas/usuarioSchema')
+const HeroRoutes = require('./routes/heroRoutes')
+const AuthRoutes = require('./routes/authRoutes')
+const JWT_SECRET = 'MEU_SEGREDAO_123'
+const app = new Hapi.Server({
+    port: 5000
+})
+
+function mapRoutes(instance, methods) {
+    return methods.map(method => instance[method]())
+}
+
+async function main() {
+    const connection = MongoDb.connect()
+    const context = new Context(new MongoDb(connection, HeroiSchema))
+    const connectionPostgres = await Postgres.connect()
+    const model = await Postgres.defineModel(connectionPostgres, UsuarioSchema)
+    const contextPostgres = new Context(new Postgres(connectionPostgres, model))
+    const swaggerOptions = {
+        info: {
+            title: 'API Herois - #CursoNodeBR',
+            version: 'v1.0'
+        }
+    }
+
+    await app.register([
+        HapiJwt,
+        Inert,
+        Vision,
+        {
+            plugin: HapiSwagger,
+            options: swaggerOptions
+        }
+    ])
+
+    app.auth.strategy('jwt', 'jwt', {
+        key: JWT_SECRET,
+        validate: async (dados, request) => {
+            const [result] = await contextPostgres.read({
+                username: dados.username.toLowerCase(),
+                id: dado.id
+            })
+
+            if (!result) {
+                return {
+                    isValid: false
+                }
+            }
+
+            return {
+                isValid: true
+            }
+        }
+    })
+
+    app.auth.default('jwt')
+    app.validator(Joi)
+    app.route([
+        ...mapRoutes(new HeroRoutes(context), HeroRoutes.methods()),
+        ...mapRoutes(new AuthRoutes(JWT_SECRET, contextPostgres), AuthRoutes.methods())
+    ])
+    await app.start()
+
+    console.log('Servidor rodando na porta', app.info.port)
+
+    return app
+}
+
+module.exports = main()
