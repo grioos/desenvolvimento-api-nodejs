5bf7417aa7222d930f093b0658799a68be7e2177
Gabriel Rios <gabriel.al.rio@gmail.com>
2020-11-12 11:05:32 -0300

    folder delete

--- 22-hapi-env/src/index.js
+++
@@ -1,9 +0,0 @@
-const ContextSrategy = require('./db/strategies/base/contextStrategy')
-const MongoDB = require('./db/strategies/mongodb')
-const Postgres = require('./db/strategies/postgres')
-
-const contextMongo = new ContextSrategy(new MongoDB())
-contextMongo.create()
-
-const contextPostgres = new ContextSrategy(new Postgres())
-contextPostgres.create()
